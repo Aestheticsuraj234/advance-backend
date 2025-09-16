bdf6d27ea4d06fbe01f6df0c10070404c3e1f516
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-09-16 16:47:12 +0530

    added

--- docker/day-1/index.js
+++ docker/day-1/index.js
@@ -1,17 +1,10 @@
 const express = require("express");
-
-
-
-const PORT = process.env.PORT || 3000;
-
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+  res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(3000, () => {
+  console.log("Example app listening on port 3000!");
 });
-
-
