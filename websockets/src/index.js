fb509ffe5876e1ff84d7775371091d83e3e56f65
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-09-25 21:04:27 +0530

    updated

--- websockets/src/index.js
+++ websockets/src/index.js
@@ -1,25 +1,24 @@
-import WebSocket , {WebSocketServer} from "ws";
+import WebSocket , { WebSocketServer} from "ws";
 import http from "http";
 
 
-const server = http.createServer((req, res) => {
-    console.log((new Date()) + " Received request for " + req.url);
+const server = http.createServer((req , res)=>{
+   console.log((new Date()) + " Received request for " + req.url);
     res.end("Hello World!");
 })
 
-
 const wss = new WebSocketServer({server});
 
-wss.on("connection", (ws) => {
-    console.log("Client connected");
-    ws.on("message", (message) => {
-        console.log("received: %s", message);
-    });
+wss.on("connection" , (ws)=>{
+    console.log("Client Connected")
+    ws.on("message" , (message)=>{
+        console.log("received: %s", message)
+        
+    })
 
-    ws.send("something");
-
+    ws.send("Hello Guys")
 })
 
-server.listen(8080, () => {
-    console.log("Server is listening on port 8080");
-})
+
+
+server.listen(8080,()=> console.log("server is running on port 8080"));
--- websockets/src/server.js
+++ websockets/src/server.js
@@ -1,19 +1,36 @@
-import express from "express";
-import { WebSocketServer } from "ws";
+import express from "express"
+import { WebSocketServer } from "ws"
 
 const app = express();
 
-const httpServer = app.listen(8080, () => {
-  console.log("Server is listening on port 8080");
-});
+const httpServer = app.listen(8080,()=> console.log("server is running on port 8080"));
 
-const wss = new WebSocketServer({ server: httpServer });
+const wss = new WebSocketServer({server:httpServer});
 
-wss.on("connection", (ws) => {
-  console.log("Client connected");
+wss.on("connection", (ws , req) => {
+
+  console.log("New Websocket connection from:" , req.socket.remoteAddress);
+
   ws.on("message", (message) => {
-    console.log("received: %s", message);
+    const data = JSON.parse(message);
+    console.log("Received:",data);
+
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify({
+            type:"broadcast",
+            message:data.message,
+            timeStamp:new Date().toISOString()
+
+        }));
+      }
+    });
   });
 
-  ws.send("something");
+
+ 
+
+  ws.on("close" , ()=>{
+    console.log("Client Disconnected")
+  })
 });
