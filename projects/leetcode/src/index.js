f4a3538b69d08b31d5d2bf8fec60a9658d077807
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-10-09 21:16:05 +0530

    added problem schema

--- projects/leetcode/src/index.js
+++ projects/leetcode/src/index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import dotenv from "dotenv"
 import morgan from "morgan";
-
-import authRoutes from "./routes/auth.routes.js"
 import cookieParser from "cookie-parser";
 
+import authRoutes from "./routes/auth.routes.js"
+import problemRoutes from "./routes/problem.routes.js"
 dotenv.config()
 
 const app = express();
@@ -25,7 +25,7 @@ app.get("/" , (req , res)=>{
 
 
 app.use("/api/v1/auth" , authRoutes);
-
+app.use("/api/v1/problems" , problemRoutes);
 
 
 app.listen(PORT , ()=>{
