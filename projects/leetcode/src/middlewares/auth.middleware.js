4112c035058f85bde969f6b87610294bb0615faa
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-10-09 22:34:55 +0530

    problem creation added

--- projects/leetcode/src/middlewares/auth.middleware.js
+++ projects/leetcode/src/middlewares/auth.middleware.js
@@ -1,13 +1,14 @@
 import jwt from "jsonwebtoken";
 import { db } from "../lib/db.js";
 
-
 export const authenticate = async (req, res, next) => {
   try {
     const token = req.cookies.jwt; // Assuming the cookie name is 'jwt'
 
     if (!token) {
-      return res.status(401).json({ message: "Unauthorized - No Token Provided" });
+      return res
+        .status(401)
+        .json({ message: "Unauthorized - No Token Provided" });
     }
 
     let decoded;
@@ -40,3 +41,28 @@ export const authenticate = async (req, res, next) => {
   }
 };
 
+export const checkAdmin = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+
+    const user = await db.user.findUnique({
+      where: {
+        id: userId,
+      },
+      select: {
+        role: true,
+      },
+    });
+
+    if (!user || user.role !== "ADMIN") {
+      return res
+        .status(403)
+        .json({ error: "Access denied. User is not an admin." });
+    }
+
+    next()
+  } catch (error) {
+        console.error("Error in checkAdmin middleware:", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
